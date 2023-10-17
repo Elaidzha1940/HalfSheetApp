bff695656b00ca33eed87212e904278b308a4640
Elaidzha1940 <eli080293@gmail.com>
2023-10-17 19:45:05 +0300

    Updated rotation3DEffect

--- HalfSheet/ContentView.swift
+++ HalfSheet/ContentView.swift
@@ -8,12 +8,32 @@
 //  */
 
 import SwiftUI
+import HalfASheet
 
 struct ContentView: View {
+    @State private var isShowing = false
+    @State private var amount = 0.0
+    
     var body: some View {
         
-        VStack {
+        ZStack {
            
+            Button(action: {
+                isShowing.toggle()
+            }, label: {
+                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+            })
+        }
+        HalfASheet(isPresented: $isShowing) {
+            VStack {
+                Image(systemName: "tortoise.fill")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold, design: .rounded))
+                    .rotation3DEffect(
+                        Angle(degrees: amount),
+                                              axis: (x: -1, y: -1, z: 0)
+                    )
+            }
         }
         .preferredColorScheme(.dark)
     }
