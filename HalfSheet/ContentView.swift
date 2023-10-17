85dfe24884b0eb37f6cf7d9e3ba7a1267b9f0834
Elaidzha1940 <eli080293@gmail.com>
2023-10-17 19:54:04 +0300

    Updated rotation3DEffect

--- HalfSheet/ContentView.swift
+++ HalfSheet/ContentView.swift
@@ -17,23 +17,38 @@ struct ContentView: View {
     var body: some View {
         
         ZStack {
-           
+            
             Button(action: {
                 isShowing.toggle()
             }, label: {
-                /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+                Text("Open")
+                    .frame(width: 100, height: 45)
+                    .font(.system(size: 25, weight: .bold, design: .rounded))
+                    .foregroundStyle(.black)
+                    .background(Color.gray)
+                    .cornerRadius(20)
+                    .padding()
+
             })
         }
-        HalfASheet(isPresented: $isShowing) {
-            VStack {
+        
+        HalfASheet(isPresented: $isShowing, title: "Rotation") {
+            VStack(spacing: 20) {
                 Image(systemName: "tortoise.fill")
                     .foregroundColor(.white)
                     .font(.system(size: 30, weight: .bold, design: .rounded))
                     .rotation3DEffect(
                         Angle(degrees: amount),
-                                              axis: (x: -1, y: -1, z: 0)
+                        axis: (x: -10, y: 1, z: 10)
                     )
+                
+                Slider(value: $amount, in: 0...300)
+                    .accentColor(.gray)
+                
+                Text("Degrees: \(Int(amount))")
+                    .font(.system(size: 20, weight: .bold, design: .rounded))
             }
+            //.padding()
         }
         .preferredColorScheme(.dark)
     }
