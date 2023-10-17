b9820c1d39e8524c81c0d177c7f019a7de8c0505
Elaidzha1940 <eli080293@gmail.com>
2023-10-17 19:33:01 +0300

    HalfSheetApp

---
+++ HalfSheet/ContentView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Project: HalfSheet
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.10.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+        .preferredColorScheme(.dark)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HalfSheet/HalfSheetApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: HalfSheet
+//  File: HalfSheetApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.10.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct HalfSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
